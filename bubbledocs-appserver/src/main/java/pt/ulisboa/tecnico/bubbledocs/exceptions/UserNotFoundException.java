4961073df97542f3b087794138646eefe945cc6a
João Carrapiço <joao.carrapico@tecnico.ulisboa.pt>
2015-03-07 16:09:24 +0000

    exception package

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/exceptions/UserNotFoundException.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/exceptions/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.bubbledocs.exceptions;
 
-public class UserNotFoundException extends Exception {
-	
+public class UserNotFoundException extends Exception {	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
 }
