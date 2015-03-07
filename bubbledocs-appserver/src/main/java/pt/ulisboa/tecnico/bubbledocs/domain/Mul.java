d7aff98b0caab6ad60df325e2926b228f69e8709
PedroSaldanha <papajorgioster@gmail.com>
2015-03-07 16:47:15 +0000

    Div ... Sub ... Mul

---
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/domain/Mul.java
@@ -0,0 +1,46 @@
+package pt.ulisboa.tecnico.bubbledocs.domain;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class Mul extends Mul_Base {
+
+    public Mul(String text, Cell arg1, Cell arg2) {
+        super();
+        super.init(text, arg1, arg2);
+    }
+
+    public Mul(String text, Cell arg1, Integer arg2) {
+        super();
+        super.init(text, arg1, arg2);
+    }
+    public Mul(String text, Integer arg1, Cell arg2) {
+        super();
+        super.init(text, arg1, arg2);
+    }
+    public Mul(String text, Integer arg1, Integer arg2) {
+        super();
+        super.init(text, arg1, arg2);
+    }
+    
+    @Override
+    public Integer getValue(){
+    	
+    	Integer arg1;
+    	Integer arg2;
+    	
+    	Set<Cell> cells = getCellsSet();
+    	Iterator<Cell> iterator = cells.iterator();
+    	
+    	arg1 = get_argument1();
+    	arg2 = get_argument2();
+    	
+    	if(arg1 == null)
+    		arg1 = iterator.next().getValue();
+    	if(arg2 == null)
+    		arg2 = iterator.next().getValue();
+    	
+    	return arg1 * arg2;
+    }
+    
+}
