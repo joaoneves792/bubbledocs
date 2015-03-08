474eb4c4f89887d4e180c66ef327aea062c8b7f2
Joao Neves <joaoneves792@gmail.com>
2015-03-08 17:37:18 +0000

    Loading the requested data at the beginning if not already there

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/BubbleApplication.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/BubbleApplication.java
@@ -3,9 +3,15 @@ package pt.ulisboa.tecnico.bubbledocs;
 
 import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
 import pt.ulisboa.tecnico.bubbledocs.domain.User;
+import pt.ulisboa.tecnico.bubbledocs.domain.Root;
 import pt.ulisboa.tecnico.bubbledocs.domain.Spreadsheet;
 import pt.ulisboa.tecnico.bubbledocs.domain.Cell;
 import pt.ulisboa.tecnico.bubbledocs.domain.Literal;
+import pt.ulisboa.tecnico.bubbledocs.domain.Reference;
+import pt.ulisboa.tecnico.bubbledocs.domain.Add;
+import pt.ulisboa.tecnico.bubbledocs.domain.Sub;
+import pt.ulisboa.tecnico.bubbledocs.domain.Mul;
+import pt.ulisboa.tecnico.bubbledocs.domain.Div;
 
 import pt.ulisboa.tecnico.bubbledocs.exceptions.*;
 
@@ -13,6 +19,7 @@ import pt.ist.fenixframework.FenixFramework;
 import pt.ist.fenixframework.TransactionManager;
 
 import javax.transaction.*;
+import java.util.Set;
 
 public class BubbleApplication{
     public static void main(String[] args){
@@ -29,6 +36,8 @@ public class BubbleApplication{
             if(bubble.getUserSet().isEmpty())
                     populateBubble(bubble);
 
+            printAllUsersInfo(bubble);
+
             tm.commit();
             committed = true;
         }catch (SystemException| NotSupportedException | RollbackException| HeuristicMixedException | HeuristicRollbackException ex) {
@@ -44,26 +53,46 @@ public class BubbleApplication{
     }
 
     private static void populateBubble(Bubbledocs bubble){
-        User user1;
-        Spreadsheet spreadsheet1;
+        Root root;
+        Spreadsheet notasEs;
         Cell cell;
 
-        System.out.println("Populating Bubbledocs");    
+        //Get the root user
+        root = Root.getRoot();
 
+        //Add two users
         try{
-            user1 = new User("Joao Neves", "joaon", "12345");
-            bubble.addUser(user1);
-            spreadsheet1 = bubble.createSpreadsheet(user1, "myDoc.doc", 5, 5);
-            cell = spreadsheet1.getCell(1,1);
-
-            cell.setContent(new Literal("99"));
-
-            System.out.println(cell.getValue());
-
-            //bubble.addWritePermission(user1, "joaon", spreadsheet1.get_id());
-        //}catch( InvalidCellException | UnauthorizedUserException | SpreadsheetNotFoundException | UserNotFoundException e){
-        }catch( BubbleCellException e){
+            root.addUser("Paul Door", "pf", "sub");
+            root.addUser("Step Rabbit", "ra", "cor");
+        }catch(UserAlreadyExistsException e){
             System.out.println(e.getMessage());
         }
+
+        //Create a Spreadsheet belonging to pf and add content to it
+        try{
+            notasEs = bubble.createSpreadsheet(bubble.getUserByUsername("pf"), "Notas ES", 300, 20);
+            cell = notasEs.getCell(3,4);
+            cell.setContent(new Literal("5"));
+            cell = notasEs.getCell(1,1);
+            cell.setContent(new Reference("=5;6", notasEs.getCell(5,6))); 
+            cell = notasEs.getCell(5,6);
+            cell.setContent(new Add("=ADD(2,3;4)", 2, notasEs.getCell(3,4)));
+            cell = notasEs.getCell(2,2);
+            cell.setContent(new Div("=DIV(1;1,3;4)", notasEs.getCell(1,1), notasEs.getCell(3,4)));
+        }catch(UserNotFoundException | InvalidCellException e){ 
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void printAllUsersInfo(Bubbledocs bubble){
+        Set<User> usersSet;
+
+        System.out.println("Users on this bubbledocs instance:");
+        System.out.println("Name | Username | Password");
+
+        usersSet = bubble.getUserSet();
+        for(User u : usersSet)
+            System.out.println(u.get_name() + " | " + u.get_username() + " | " + u.get_passwd());
+
     }
 }
