dfb295d5dd429b189a81d72ea859f9898bbd69f8
João Carrapiço <joao.carrapico@tecnico.ulisboa.pt>
2015-03-09 12:27:33 +0000

    changed domain to facilitate import/export methods WARNING: run $mvn generate-sources to change the _base classes

---
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/domain/SimpleContent.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.bubbledocs.domain;
+
+public class SimpleContent extends SimpleContent_Base {
+    
+    public SimpleContent() {
+        super();
+    }
+    
+    protected 
+    
+}
