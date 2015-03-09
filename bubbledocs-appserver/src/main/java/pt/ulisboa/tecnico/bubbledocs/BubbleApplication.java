07d3af588969189bfeb4167cb03039ac0093406f
Joao Neves <joaoneves792@gmail.com>
2015-03-09 20:58:09 +0000

    Updates to main

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/BubbleApplication.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/BubbleApplication.java
@@ -12,6 +12,7 @@ import pt.ulisboa.tecnico.bubbledocs.domain.Add;
 import pt.ulisboa.tecnico.bubbledocs.domain.Sub;
 import pt.ulisboa.tecnico.bubbledocs.domain.Mul;
 import pt.ulisboa.tecnico.bubbledocs.domain.Div;
+import pt.ulisboa.tecnico.bubbledocs.domain.SimpleContent;
 
 import pt.ulisboa.tecnico.bubbledocs.exceptions.*;
 
@@ -39,48 +40,62 @@ public class BubbleApplication{
                     populateBubble(bubble);
 
             //Print all users names, usernames, and passords
-            printAllUsersInfo(bubble);
+            //printAllUsersInfo(bubble);
 
             //Print which documents each user owns (owns as in author)
-            try{
+            /*try{
                 printUsersDocuments(bubble, "pf");
                 printUsersDocuments(bubble, "ra");
             }catch(UserNotFoundException e){
                 System.out.println(e.getMessage());
-            }
+            }*/
 
-            //Export to XML all documents belonging to "pf"
-            exportUsersDocuments(bubble, "pf");
+            /*Export to XML all documents belonging to "pf"
+            try{
+                exportUsersDocuments(bubble, "pf");
+            }catch(UserNotFoundException e){
+                System.out.println("Failed to export document: " + e.getMessage());
+            }*/
 
             //Remove pf's "Notas ES" spreadsheet from storage
-            //TODO This is not actually removing the data from the database!!!!!
-            spreadsheet = getSpreadsheetByName("Notas ES", bubble.getSpreadsheetsByAuthor("pf"));
+            //TODO This might not  actually be removing the data from the database!!!!!
+            //TODO Crashes with InvalidStateException
+            /*spreadsheet = getSpreadsheetByName("Notas ES", bubble.getSpreadsheetsByAuthor("pf"));
             if(null == spreadsheet)
                 System.out.println("User pf doesnt have a document named Notas ES");
             else{
+                spreadsheet.clean();
                 bubble.removeSpreadsheet(spreadsheet);
                 spreadsheet = null;
-            }
+            }*/
 
 
             //Print all the documents owned by "pf" 
-            try{
+            /*try{
                 printUsersDocuments(bubble, "pf");
             }catch(UserNotFoundException e){
                 System.out.println(e.getMessage());
-            }
+            }*/
 
             //TODO: Import the spreadsheet we just exported
  
-            //Print all the documents owned by "pf" again...
+            /*Print all the documents owned by "pf" again...
             try{
                 printUsersDocuments(bubble, "pf");
             }catch(UserNotFoundException e){
-                System.out.println(e.getMessage());
-            }   
+                System.out.println("Failed to print user documents: " + e.getMessage());
+            } */  
 
-            //Export to XML the document again...        
-            exportUsersDocuments(bubble, "pf");
+            /*Export to XML the document again...        
+            try{
+                exportUsersDocuments(bubble, "pf");
+            }catch(UserNotFoundException e){
+                System.out.println("Failed to export document: " + e.getMessage());
+            } */  
+
+            //This is a temporary function just to test things
+            //TODO REMOVE ME
+            testThings(bubble);
 
             tm.commit();
             committed = true;
@@ -98,10 +113,26 @@ public class BubbleApplication{
         FenixFramework.shutdown();
     }
 
+    public static void testThings(Bubbledocs bubble){
+        try{
+            Spreadsheet notasEs = bubble.getSpreadsheetById(0);
+
+            System.out.println("Cell 1;1 : " + notasEs.getCell(1,1).getValue() );   
+            System.out.println("Cell 2;2 : " + notasEs.getCell(2,2).getValue() );   
+            System.out.println("Cell 3;4 : " + notasEs.getCell(3,4).getValue() );   
+            System.out.println("Cell 5;6 : " + notasEs.getCell(5,6).getValue() );   
+        }catch(BubbleCellException | SpreadsheetNotFoundException e){
+            System.out.println(e.getMessage());
+        } 
+    }
+
     private static void populateBubble(Bubbledocs bubble){
         Root root;
         Spreadsheet notasEs;
-        Cell cell;
+        Cell cell1;
+        Cell cell2;
+        Cell cell3;
+        Cell cell4;
 
         //Get the root user
         root = Root.getRoot();
@@ -116,15 +147,49 @@ public class BubbleApplication{
 
         //Create a Spreadsheet belonging to pf and add content to it
         try{
+            Literal lit;
+            Literal lit2;
+            Reference ref;
+            Reference ref2;
+            Reference ref3;
+            Reference ref4;
+            Add add;
+            Div div;
+   
             notasEs = bubble.createSpreadsheet(bubble.getUserByUsername("pf"), "Notas ES", 300, 20);
-            cell = notasEs.getCell(3,4);
-            cell.setContent(new Literal("5"));
-            cell = notasEs.getCell(1,1);
-            cell.setContent(new Reference("=5;6", notasEs.getCell(5,6))); 
-            cell = notasEs.getCell(5,6);
-            cell.setContent(new Add("=ADD(2,3;4)", 2, notasEs.getCell(3,4)));
-            cell = notasEs.getCell(2,2);
-            cell.setContent(new Div("=DIV(1;1,3;4)", notasEs.getCell(1,1), notasEs.getCell(3,4)));
+            
+            //Literal 5 in 3;4
+            cell1 = notasEs.getCell(3,4);
+            lit = new Literal();
+            lit.init(5);
+            cell1.setContent(lit);
+            
+            //Reference to 5;6 IN 1;1
+            cell2 = notasEs.getCell(1,1);
+            ref = new Reference();
+            ref.init(notasEs.getCell(3,4));
+            cell2.setContent(ref);
+    
+            //Add 2 and 3;4 in 5;6
+            cell3 = notasEs.getCell(5,6);
+            add = new Add();
+            lit2 = new Literal();
+            lit2.init(2);
+            ref2 = new Reference();
+            ref2.init(notasEs.getCell(3,4));
+            add.init(lit2, ref2);
+            cell3.setContent(add);
+            
+            //Div 1;1 by 3;4 in 2;2
+            cell4 = notasEs.getCell(2,2);
+            div = new Div();
+            ref3 = new Reference();
+            ref3.init(notasEs.getCell(1,1));
+            ref4 = new Reference();
+            ref4.init(notasEs.getCell(3,4));
+            div.init(ref3, ref4);
+            cell4.setContent(div);   
+    
         }catch(UserNotFoundException | InvalidCellException e){ 
             System.out.println(e.getMessage());
         }
