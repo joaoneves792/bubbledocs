5032677701168d440d37a4ec150107acefeb91df
Pedro Saldanha <papajorgioster@gmail.com>
2015-03-15 17:04:40 +0000

    ExpiredSessionException

---
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/exceptions/ExpiredSessionException.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.tecnico.bubbledocs.exceptions;
+
+public class ExpiredSessionException extends BubbledocsException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExpiredSessionException(String m) {
+		super(m);
+	}
+}
