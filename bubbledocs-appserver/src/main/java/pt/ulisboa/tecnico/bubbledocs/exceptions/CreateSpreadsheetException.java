dccac87c00af99062c79ec25c8a989ec24789785
João Carrapiço <joao.carrapico@tecnico.ulisboa.pt>
2015-03-22 19:38:03 +0000

    another one lol\

---
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/exceptions/CreateSpreadsheetException.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.tecnico.bubbledocs.exceptions;
+
+public class CreateSpreadsheetException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CreateSpreadsheetException(String m) {
+		super(m);
+	}
+}
