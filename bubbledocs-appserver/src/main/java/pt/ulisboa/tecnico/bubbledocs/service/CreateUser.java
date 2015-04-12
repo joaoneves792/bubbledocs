c4f6a2328c3f80f790ff6246909726f4e7275560
João Carrapiço <joao.carrapico@tecnico.ulisboa.pt>
2015-04-12 16:54:32 +0100

    closes #12

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/CreateUser.java
@@ -2,14 +2,20 @@ package pt.ulisboa.tecnico.bubbledocs.service;
 
 import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
 import pt.ulisboa.tecnico.bubbledocs.domain.Root;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.DuplicateEmailException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.DuplicateUsernameException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.EmptyNameException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.EmptyPasswordException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.EmptyUsernameException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.InvalidEmailException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.InvalidUsernameException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.RemoteInvocationException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UnauthorizedUserException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.UnavailableServiceException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UserAlreadyExistsException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UserNotFoundException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UserNotInSessionException;
+import pt.ulisboa.tecnico.bubbledocs.service.remote.IDRemoteServices;
 
 // add needed import declarations
 
@@ -19,12 +25,12 @@ public class CreateUser extends BubbledocsService {
 	private String email;
 	private String name;
 
-    public CreateUser(String rootTok, String username, String email, String name) throws EmptyPasswordException, EmptyUsernameException, EmptyNameException, InvalidUsernameException {
+    public CreateUser(String rootTok, String username, String email, String name) throws EmptyPasswordException, EmptyUsernameException, EmptyNameException, InvalidUsernameException, InvalidEmailException {
 
     	if(username == null || username.isEmpty())
-    		throw new EmptyUsernameException("Attempted to create user with no username");
+    		throw new InvalidUsernameException("Attempted to create user with no username");
     	else if(email == null || email.isEmpty()) {
-    		throw new EmptyPasswordException("Attempted to create a user with no password.");
+    		throw new InvalidEmailException("Attempted to create a user with no email.");
     	} else if(name == null || name.isEmpty()) {
     		throw new EmptyNameException("Attempted to create a user with no name.");
     	} else if(username.length() < 3 || username.length() > 8) {
@@ -38,9 +44,18 @@ public class CreateUser extends BubbledocsService {
     }
 
     @Override
-    protected void dispatch() throws UnauthorizedUserException, UserAlreadyExistsException, UserNotInSessionException, EmptyPasswordException, EmptyUsernameException, UserNotFoundException {
+    protected void dispatch() throws UnauthorizedUserException, UserAlreadyExistsException, UserNotInSessionException, EmptyPasswordException, EmptyUsernameException, UserNotFoundException, InvalidUsernameException, DuplicateUsernameException, DuplicateEmailException, InvalidEmailException, UnavailableServiceException {
     	if(!rootToken.matches("root\\d"))
     		throw new UnauthorizedUserException("The user in session ["+ rootToken + "] is not authorized to create new users.");
+
+    	IDRemoteServices sdId = new IDRemoteServices();
+    	
+    	try {
+    		sdId.createUser(newUsername, email);
+    	} catch (RemoteInvocationException e) {
+    		throw new UnavailableServiceException("SD-ID is offline.");
+    	}
+    	
     	((Root)Bubbledocs.getBubbledocs().getUserByUsername("root")).addUser(name, newUsername, email);
     }
 
