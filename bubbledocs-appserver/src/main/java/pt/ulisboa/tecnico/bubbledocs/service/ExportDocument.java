955defde323ea5b05beca17899a550b420355c24
João Carrapiço <joao.carrapico@tecnico.ulisboa.pt>
2015-04-12 19:32:26 +0100

    closes #21

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/ExportDocument.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/ExportDocument.java
@@ -2,6 +2,9 @@ package pt.ulisboa.tecnico.bubbledocs.service;
 
 import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.BubbledocsException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.RemoteInvocationException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.UnavailableServiceException;
+import pt.ulisboa.tecnico.bubbledocs.service.remote.StoreRemoteServices;
 
 // add needed import declarations
 
@@ -22,7 +25,16 @@ public class ExportDocument extends BubbledocsService {
     @Override
     protected void dispatch() throws BubbledocsException {
         Bubbledocs bubble = Bubbledocs.getBubbledocs();
+        
+        StoreRemoteServices sdStore = new StoreRemoteServices();
+        
         docXML = bubble.exportDocument(userToken, docId);
+        String ssName = Bubbledocs.getBubbledocs().getSpreadsheetById(docId).getName();
+        try {
+        	sdStore.storeDocument(userToken.split("\\d")[0], ssName, docXML.getBytes());
+        } catch (RemoteInvocationException e) {
+        	throw new UnavailableServiceException("SD-Store offline.");
+        }
     }
     
 }
