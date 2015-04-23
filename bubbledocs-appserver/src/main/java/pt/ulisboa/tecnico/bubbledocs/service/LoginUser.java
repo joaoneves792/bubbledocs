7926cd8b6b0efa79f01b95b2eb7b8e6f13c4979a
Joao Neves <joaoneves792@gmail.com>
2015-04-23 11:51:33 +0100

    closes #57

--- bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/LoginUser.java
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/LoginUser.java
@@ -3,24 +3,16 @@ package pt.ulisboa.tecnico.bubbledocs.service;
 import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.InvalidLoginException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.LoginBubbleDocsException;
-import pt.ulisboa.tecnico.bubbledocs.exceptions.RemoteInvocationException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UnavailableServiceException;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UserNotFoundException;
-import pt.ulisboa.tecnico.bubbledocs.service.remote.IDRemoteServices;
 import pt.ulisboa.tecnico.bubbledocs.exceptions.UserNotInSessionException;
 
-// add needed import declarations
 
 public class LoginUser extends BubbledocsService {
 
     private String username;
     private String password;
 
-    /**
-     * Perform a user login
-     * @param string with a username
-     * @param string with the users password
-     */
     public LoginUser(String usrname, String passwd) {
         username = usrname;
         password = passwd;
@@ -32,34 +24,29 @@ public class LoginUser extends BubbledocsService {
     }
     
     @Override
-    protected void dispatch() throws UnavailableServiceException, LoginBubbleDocsException {
-        Bubbledocs bubble;
-        IDRemoteServices sdId;
-        
-        bubble = Bubbledocs.getBubbledocs();
-        sdId = new IDRemoteServices();
+    protected void dispatch() throws UnavailableServiceException, LoginBubbleDocsException{
+        Bubbledocs bubble = Bubbledocs.getBubbledocs();
         
         try{
-        	sdId.loginUser(username, password);
-        }catch(RemoteInvocationException e){
-        	try{
-        		bubble.localLogin(username, password);
-        	}catch(UserNotFoundException | InvalidLoginException lle){
-        		throw new UnavailableServiceException("The service is unavailable.");
-        	}
+        	bubble.localLogin(username, password);
+        }catch(UserNotFoundException | InvalidLoginException lle){
+        	throw new UnavailableServiceException("The service is unavailable.");
         }
-        //If we reach here then login was successful
-        
-        try{
+        createSession();
+    }
+
+    public void createSession() throws LoginBubbleDocsException{
+        Bubbledocs bubble = Bubbledocs.getBubbledocs();
+
+    	try{
         	bubble.updateLocalPassword(username, password);
-    	
-        	userToken = bubble.createSession(username);
+    	   	userToken = bubble.createSession(username);
         }catch(UserNotFoundException e){
         	//This should only happen if the user was deleted on the domain but still managed to login remotely!
         	throw new LoginBubbleDocsException("Login failed");
         }
     }
-
+    
     public final String getUserToken() {
     	return userToken;
     }
