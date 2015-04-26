bea1b43278b4c749920cda82fae7eb742d0520e3
João Carrapiço <joao.carrapico@tecnico.ulisboa.pt>
2015-04-26 16:43:56 +0100

    closes #33

---
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/GetSpreadsheetContent.java
@@ -0,0 +1,25 @@
+package pt.ulisboa.tecnico.bubbledocs.service;
+
+import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.BubbledocsException;
+
+public class GetSpreadsheetContent extends BubbledocsService {
+
+	private String result;
+	private final int spreadsheetID;
+	
+	public GetSpreadsheetContent(int spreadsheetID) {
+		this.spreadsheetID = spreadsheetID;
+		result = "";
+	}
+	
+	@Override
+	protected void dispatch() throws BubbledocsException {
+		result = Bubbledocs.getBubbledocs().getSpreadsheetById(spreadsheetID).toString();
+	}
+	
+	public String getResult() {
+		return result;
+	}
+
+}
