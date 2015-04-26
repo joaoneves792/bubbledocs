914afc55b364fe5c0949e46df1752f08808dde50
PedroSaldanha <papajorgioster@gmail.com>
2015-04-26 18:25:48 +0100

    closes #53

---
+++ bubbledocs-appserver/src/main/java/pt/ulisboa/tecnico/bubbledocs/service/integrator/ExportDocumentIntegrator.java
@@ -0,0 +1,40 @@
+package pt.ulisboa.tecnico.bubbledocs.service.integrator;
+
+import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.BubbledocsException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.RemoteInvocationException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.UnavailableServiceException;
+import pt.ulisboa.tecnico.bubbledocs.service.ExportDocument;
+import pt.ulisboa.tecnico.bubbledocs.service.remote.StoreRemoteServices;
+
+	public class ExportDocumentIntegrator extends BubbledocsIntegrator {
+
+		private int docId;
+		private String docXML;
+	    
+	    public ExportDocumentIntegrator(String userTok, int ssId) {
+	    	userToken=userTok;
+	    	docId=ssId;
+	    }
+	    
+	    @Override
+	    protected void dispatch() throws BubbledocsException {
+	    	
+	        StoreRemoteServices sdStore = new StoreRemoteServices();
+	        ExportDocument service = new ExportDocument(userToken, docId);
+	        
+	        String ssName = Bubbledocs.getBubbledocs().getSpreadsheetById(docId).getName();
+	        
+	        try {
+	        	service.execute();
+	        	docXML = service.getDocXML();
+	        	sdStore.storeDocument(userToken.split("\\d")[0], ssName, docXML.getBytes());
+	        } catch (RemoteInvocationException e) {
+	        	throw new UnavailableServiceException("SD-Store offline.");
+	        }
+	    }
+	    
+	    public final String getDocXML() {
+		    return docXML;
+	    }
+	}
