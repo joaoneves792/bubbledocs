d0cc5ebbb133575f2d1f8bde832c15fcf363b4a8
Joao Neves <joaoneves792@gmail.com>
2015-05-08 19:12:36 +0100

    closes #82

---
+++ bubbledocs-appserver/src/test/java/pt/ulisboa/tecnico/bubbledocs/service/integration/system/SystemTest.java
@@ -0,0 +1,25 @@
+package pt.ulisboa.tecnico.bubbledocs.service.integration.system;
+
+import org.junit.After;
+import org.junit.Before;
+
+import pt.ulisboa.tecnico.bubbledocs.domain.Bubbledocs;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.BubbledocsException;
+import pt.ulisboa.tecnico.bubbledocs.exceptions.InvalidUsernameException;
+
+public abstract class SystemTest {
+
+    @Before
+    public void setUp()throws BubbledocsException {
+    	try {
+			Bubbledocs.getBubbledocs().getSuperUser();
+		} catch (InvalidUsernameException e) {
+			throw new BubbledocsException("Unable to create the super user!");
+		}
+    }
+
+    @After
+    public void tearDown() throws BubbledocsException {
+    	Bubbledocs.getBubbledocs().clean();
+    }
+}
